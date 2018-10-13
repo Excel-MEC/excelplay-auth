8ee18b14a882fefa0cde44bf8095aff04ee9a3dd
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-13 12:37:50 +0530

    Update docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,4 @@
 
 python excelplay-auth/excelplay_auth/manage.py makemigrations && \
 	python excelplay-auth/excelplay_auth/manage.py migrate
-python excelplay-auth/excelplay_auth/manage.py runserver 0.0.0.0:8000
+gunicorn excelplay-auth/excelplay_auth/excelplay_auth.wsgi --bind 0.0.0.0:8000
