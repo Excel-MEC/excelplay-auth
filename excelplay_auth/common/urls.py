3963282e00df0ee525a6b7d852f90654aaa3ac01
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-15 22:26:24 +0530

    Add url for get_all_users

--- excelplay_auth/common/urls.py
+++ excelplay_auth/common/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import sign_in, sign_out, set_token, get_user_detail
+from .views import sign_in, sign_out, set_token, get_user_detail, get_all_users
 
 urlpatterns = [
         path('signin', sign_in, name='sign_in'),
         path('signout', sign_out, name='sign_out'),
         path('token', set_token, name='set_token'),
-        path('user/detail', get_user_detail, name='get_user_detail')
+        path('user/detail', get_user_detail, name='get_user_detail'),
+        path('users', get_all_users, name='get_all_users')
 ]
