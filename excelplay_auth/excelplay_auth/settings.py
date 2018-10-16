8677adaa9a4470dc1d90162d5e3df2ebb7d1d07d
augustine <augustinetharakan12@gmail.com>
2018-10-16 10:46:21 +0530

    get user detail session

--- excelplay_auth/excelplay_auth/settings.py
+++ excelplay_auth/excelplay_auth/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD':'123456789',
         'HOST': 'localhost',
         'PORT': 5432,
     }
