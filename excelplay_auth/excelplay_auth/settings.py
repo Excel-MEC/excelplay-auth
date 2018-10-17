a3d2eda0ce392578aaf33bc5a69fbe1db81c0903
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-17 19:35:43 +0530

    Update staticfiles settings

--- excelplay_auth/excelplay_auth/settings.py
+++ excelplay_auth/excelplay_auth/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+        ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -127,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # CORS Config
 CORS_ORIGIN_ALLOW_ALL = True
