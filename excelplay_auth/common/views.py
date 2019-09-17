8ec2d5962f080154a7cb22cbfd2d55844dd84c56
Augustine <augustinetharakan12@gmail.com>
2019-09-17 19:09:57 +0530

    updated auth0 userinfo endpoint url

--- excelplay_auth/common/views.py
+++ excelplay_auth/common/views.py
@@ -33,7 +33,7 @@ def sign_in(request):
         try:
 
             headers = {'Authorization': 'Bearer % s' % access_token}
-            r = requests.get('https://excelplay2018.auth0.com/userinfo',
+            r = requests.get('http://ajeshkumar.eu.auth0.com//userinfo',
                              headers=headers)
 
             userinfo = r.json()
