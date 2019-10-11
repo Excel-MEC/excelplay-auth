3dc6b146bbd77c8635a1cd69b9ab416f9bebf20e
Augustine <augustinetharakan12@gmail.com>
2019-10-11 18:55:35 +0530

    admin dashboard changes, added search field, list display

--- excelplay_auth/common/admin.py
+++ excelplay_auth/common/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import *
+from .models import User
 # Register your models here.
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'profile_picture')
+    search_fields = ('name',)
+
+
+admin.site.register(User, UserAdmin)
